afafef5b83f4bf08b537a6db51f4e75930970ba1
Nigel <m0cvo@yahoo.co.uk>
2022-03-23 08:23:42 +0000

    Add ILandBound Interface

--- Program.cs
+++ Program.cs
@@ -3,32 +3,41 @@
 namespace InheritanceDemo
 {
     /// <summary>
-    /// Firstly create Mammal as the base class containing methods that are common to all mammals
+    /// Firstly create ILandBound as an interface that defines an object to be used later in the classes
+    /// </summary>
+    interface ILandBound
+    {
+        int NumberOfLegs();
+    }
+    /// <summary>
+    /// Then we create the class Mammal which will be the base class....
     /// </summary>
     class Mammal
     {
         public void Breathe()
         {
-            Console.WriteLine("All mammals breathe \n");
+            Console.WriteLine("All mammals breathe ");
         }
-
         public void SuckleYoung()
         {
             Console.WriteLine("All mammals suckle their young \n");
         }
     }
-
     /// <summary>
-    /// Then declare derived classes for different types of mammals that have methods uniqu to that type of animal
+    /// Then declare derived classes for different types of mammals that have methods unique to that type of animal
+    /// The horse class also inherits from the ILandBound interface
     /// </summary>
-    class Horse : Mammal
+    class Horse : Mammal, ILandBound
     {
+        int ILandBound.NumberOfLegs()
+        { 
+            return 4; 
+        }
         public void Trot()
         {
             Console.WriteLine("Horses also trot \n");
         }
     }
-
     class Whale : Mammal
     {
         public void Swim()
@@ -36,7 +45,6 @@ namespace InheritanceDemo
             Console.WriteLine("Whales swim \n");
         }
     }
-
     /// <summary>
     /// The Program class contains the Main method which controls the flow of the program
     /// </summary>
@@ -52,6 +60,12 @@ namespace InheritanceDemo
             
             myHorse.Breathe();
             myHorse.SuckleYoung();
+
+            //The ILandBound interface must be explicitly implemented.
+            ILandBound landBound = myHorse;
+            int legsOnHorse = landBound.NumberOfLegs();
+            Console.WriteLine("A horse has " + legsOnHorse + " legs.");
+
             myHorse.Trot();
             Console.WriteLine("\n");
             Whale myWhale = new Whale();
